4c5ee5339aa40b85ab371143f270a7e946e07849
kevinwtl <tinglam@TingLams-MacBook-Pro.local>
2020-08-31 13:59:07 +0800

    .

--- pricing.py
+++ pricing.py
@@ -5,6 +5,9 @@ import scipy.stats as si
 from scipy.stats import norm
 import random
 import matplotlib.pyplot as plt
+'''
+TODO: payoff of shorted options are not correctly reflected
+'''
 
 
 class black_scholes:
@@ -64,16 +67,21 @@ class black_scholes:
         return self.S * self.delta / self.S
 
     def calculate(self):
-        print("Price: " + str("{:.2f}".format(self.price)))
-        print("Delta: " + str("{:.2f}".format(self.delta)))
-        print("Gamma: " + str("{:.2f}".format(self.gamma)) + " (per % of Spot)")
-        print("Vega: " + str("{:.2f}".format(self.vega)) + " (per % of IV)")
-        print("Theta: " + str("{:.2f}".format(self.theta)) + " (daily)")
-        print("Rho: " + str("{:.2f}".format(self.rho)) + " (per % of Interest Rate)")
+        print("Strategy: " + self.strategy_name)
+        print("Price: " + str("{:.3f}".format(self.price)))
+        print("Delta: " + str("{:.3f}".format(self.delta)))
+        print("Gamma: " + str("{:.3f}".format(self.gamma)) + " (per % of Spot)")
+        print("Vega: " + str("{:.3f}".format(self.vega)) + " (per % of IV)")
+        print("Theta: " + str("{:.3f}".format(self.theta)) + " (daily)")
+        print("Rho: " + str("{:.3f}".format(self.rho)) + " (per % of Interest Rate)")
+        print("Leverage: " + str("{:.3f}".format(self.leverage)))
+        print("Exposure: " + str("{:.3f}".format(self.exposure)))
 
 class call(black_scholes):
     def __init__(self, S, K, T, r, q, sigma):
         super().__init__(S, K, T, r, q, sigma)
+        self.strategy_name = 'Call @ K = ' + str(K)
+        
         self.price = black_scholes.price(self,'call')
         self.delta = black_scholes.delta(self,'call')
         self.gamma = black_scholes.gamma(self)
@@ -82,10 +90,18 @@ class call(black_scholes):
         self.rho = black_scholes.rho(self,'call')
         self.leverage = black_scholes.leverage(self)
         self.exposure = black_scholes.exposure(self)
+        
+    def payoff(self,St):
+        return np.where(St > self.S, St - self.S, 0) - self.price
+    
+    def profit(self,St):
+        return np.where(St > self.S, St - self.S, 0)
 
 class put(black_scholes):
     def __init__(self, S, K, T, r, q, sigma):
         super().__init__(S, K, T, r, q, sigma)
+        self.strategy_name = 'Put @ K = ' + str(K)
+        
         self.price = black_scholes.price(self,'put')
         self.delta = black_scholes.delta(self,'put')
         self.gamma = black_scholes.gamma(self)
@@ -95,80 +111,106 @@ class put(black_scholes):
         self.leverage = black_scholes.leverage(self)
         self.exposure = black_scholes.exposure(self)
 
-class strategy(call,put):
-    def __init__(self, options = [], strategy = ''):
+    def payoff(self,St):
+        return np.where(St < self.S, self.S - St, 0)
+    
+    def profit(self,St):
+        return np.where(St < self.S, self.S - St, 0) - self.price
+
+class straddle(call,put):
+    def __init__(self, S, K, T, r, q, sigma):
         super().__init__(S, K, T, r, q, sigma)
+        self.strategy_name = 'Straddle @ K = ' + str(K)
+        
+        option_1 = call(S, K, T, r, q, sigma) # Long Call
+        option_2 = put(S, K, T, r, q, sigma) # Long Put
+        
+        self.option_1 = option_1
+        self.option_2 = option_2
+        self.price = option_1.price + option_2.price
+        self.delta = option_1.delta + option_2.delta
+        self.gamma = option_1.gamma + option_2.gamma
+        self.vega = option_1.vega + option_2.vega
+        self.theta = option_1.theta + option_2.theta
+        self.rho = option_1.rho + option_2.rho
+        self.leverage = option_1.leverage + option_2.leverage
+        self.exposure = option_1.vega + option_2.vega
+        
+    def payoff(self,St):
+        return self.option_1.payoff(St) + self.option_2.payoff(St)
+    
+    def profit(self,St):
+        return self.option_1.payoff(St) + self.option_2.payoff(St) - self.price
 
-        # Distinguish between call / put options
-        call_count, put_cout, calls, puts = 0,0,[],[]
-        for i in options:
-            if i.__class__.__name__ == 'call':
-                calls.append(i)
-            elif i.__class__.__name__ == 'put':
-                puts.append(i)
-
-
-        if strategy == 'straddle':
-            self.price = calls[0].price + puts[0].price
-            self.delta = calls[0].delta + puts[0].delta
-            self.gamma
-
-        if strategy == ''
-
-
-def strategy(S1,K,T1,r,q,sigma): #Straddle
-    long_call = call(S1,K,T1,r,q,sigma)
-    long_put = put(S1,K,T1,r,q,sigma)
-    price = long_call.price + long_put.price
-
-    return price
-
-
-def montecarlo(option, n):
-    # Only for long options currently
-    sns.set_style('whitegrid')
-    pct_chg = np.random.normal(1, option.sigma * option.T, n)
-    ms_S = pct_chg * option.S
-    ms_payoff =  ms_S - option.K - option.price
-    ms_payoff[ms_payoff<0] = 0
-
-    return pd.DataFrame({'St' : ms_S, 'Payoff' : ms_payoff})
-
-
-def payoff_diagram(df):
-
-    t = np.arange(0,len(df)) # number of values on the x axis
-    fig, ax = plt.subplots() # Create the plot objects
-    ax.scatter(df['St'], df['Payoff'],s = 5) # Input the data for the graph and plot
-    ax.set_xlabel('St')
-    ax.set_ylabel('Payoff')
-
-    plt.show()
-
-
-
-
-
-
+class spread(call,put):
+    '''
+    K1 < K2 means a bull spread.
+    Always long K1, short K2
+    option_type = call or put
+    '''
+    def __init__(self, S, K1, K2, T, r, q, sigma, option_type):
+        super().__init__(S, K, T, r, q, sigma)
+        
+        if option_type == 'call':
+            self.strategy_name = 'Call Spread @ K1 = ' + str(K1) + ', K2 = ' + str(K2)
+            option_1 = call(S, K1, T, r, q, sigma) # Long call
+            option_2 = call(S, K2, T, r, q, sigma) # Short call
+        elif option_type == 'put':
+            self.strategy_name = 'Put Spread @ K1 = ' + str(K1) + ', K2 = ' + str(K2)
+            option_1 = put(S, K1, T, r, q, sigma) # Long Put
+            option_2 = put(S, K2, T, r, q, sigma) # Short Put
+        
+        self.option_1 = option_1
+        self.option_2 = option_2        
+        self.price = option_1.price - option_2.price
+        self.delta = option_1.delta - option_2.delta
+        self.gamma = option_1.gamma - option_2.gamma
+        self.vega = option_1.vega - option_2.vega
+        self.theta = option_1.theta - option_2.theta
+        self.rho = option_1.rho - option_2.rho
+        self.leverage = option_1.leverage - option_2.leverage
+        self.exposure = option_1.vega - option_2.vega
+        
+    def payoff(self,St):
+        return self.option_1.payoff(St) - self.option_2.payoff(St)
+    
+    def profit(self,St):
+        return self.option_1.payoff(St) - self.option_2.payoff(St) - self.price
 
 
 # Options at interception
-S,K,T,r,q,sigma = 18.8,18.5,75/365,0.003,0,0.2605
+S,K,T,r,q,sigma = 23.65,24,29/365,0.003,0,0.6951
 
-
+option = straddle(S=23.65,K=24,T=29/365,r=0.03,q=0,sigma=0.7)
+option = spread(S=23.65,K1=23.5,K2=24,T=29/365,r=0.03,q=0,sigma=0.7,option_type='put')
 
 long_put = put(S,K,T,r,q,sigma)
 long_call = call(S,K,T,r,q,sigma)
 
-strategy([long_put, long_call]).price
+
 
 
 ## Payoff diagram
-df = montecarlo(long_put,0.1,500)
-
-payoff_diagram(df)
 
 
+def payoff_diagram(option, n):
+
+    def montecarlo(option, n):
+        # Only for long options currently
+        sns.set_style('whitegrid')
+        pct_chg = np.random.normal(1, option.sigma * option.T, n)
+        ms_S = pct_chg * option.S
+        ms_profit = option.profit(ms_S)
+        return pd.DataFrame({'St' : ms_S, 'Profit' : ms_profit})
+    
+    df = montecarlo(option, n)
+    t = np.arange(0,len(df)) # number of values on the x axis
+    fig, ax = plt.subplots() # Create the plot objects
+    ax.scatter(df['St'], df['Profit'],s = 5) # Input the data for the graph and plot
+    ax.set_xlabel('St')
+    ax.set_ylabel('Profit')
+
+    plt.show()
 
 
-
+payoff_diagram(spread(S=24.65,K1=22,K2=26,T=29/365,r=0.03,q=0,sigma=0.3,option_type='call'),n = 2500)
