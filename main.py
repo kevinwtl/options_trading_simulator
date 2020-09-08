2a737a25a83c073704771e076ffd7f738ed230fa
kevinwtl <tinglam@TingLams-MacBook-Pro.local>
2020-09-08 16:19:52 +0800

    .

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import os
 os.chdir('/Users/tinglam/Documents/GitHub/options_trading_simulator')
 import bsm
 import option_data
-import matplotlib.pyplot as plt
 
 
 
 # 700 HK Call
-S = 520
+S = 525
 K = 530
-T = 22/365
+T = 21/365
 r = 0
 q = 0
-IV = 0.43
+IV = 0.3854
 
 x = bsm.call(S,K,T,r,q,IV,position='long')
 
@@ -25,10 +24,10 @@ bsm.get_simulation(option = x,expected_price = 515, std = 0.036, skew = 0, n = 2
 # 1810 HK Call
 S = 24.6
 K = 26
-T = 22/365
+T = 21/365
 r = 0
 q = 0
-IV = 0.75
+IV = 0.6761
 
 x = bsm.call(S,K,T,r,q,IV,position='long')
 
@@ -37,6 +36,37 @@ x.describe()
 bsm.get_simulation(option = x,expected_price = 25, std = 0.03, skew = -2, n = 25000, Tt = 21/365)
 
 
+
+# Short 700 HK Put
+S = 505
+K = 480
+T = 21/365
+r = 0
+q = 0
+IV = 0.385
+
+x = bsm.put(S,K,T,r,q,IV,position='short')
+
+x.describe()
+
+bsm.get_simulation(option = x,expected_price = 505, std = 0.036, skew = 0, n = 2500, Tt = 19/365)
+
+
+
+
+# Short Straddle
+S = 24.6
+K = 22
+T = 21/365
+r = 0
+q = 0
+IV = 0.75
+
+x = bsm.straddle(S,K,T,r,q,IV,position='short')
+
+x.describe()
+
+bsm.get_simulation(option = x,expected_price = 25, std = 0.03, skew = -2, n = 25000, Tt = 21/365)
 
 
 
